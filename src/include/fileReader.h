691e0a60c14a67a77770e9a0db751d91d2666903
Sozonov Andrey <andreysoz@yandex.ru>
2022-11-14 21:22:43 +0300

    feat: update file open and reading functions

--- src/include/fileReader.h
+++ src/include/fileReader.h
@@ -2,15 +2,24 @@
 #define FILEREADER_H
 #include <filesystem>
 #include <fstream>
-#include <iostream>
 #include <string>
 #include <vector>
+#include <iostream>
 
-std::string read_file_chars( const std::filesystem::path& path, std::uint32_t numb)
+static std::ifstream open_file( const std::filesystem::path& filepath, std::ostream & info = std::cout ) {
+    if( not std::filesystem::exists( filepath ) )
+          throw std::invalid_argument{ "File: " + filepath.string() + " does not exist." };
+
+      if( not std::filesystem::is_regular_file( filepath ) )
+          throw std::invalid_argument{"File: " + filepath.string() + " is not a file."};
+
+      info << "File: " + filepath.string() + " is valid." << std::endl;
+
+      return std::ifstream(filepath, std::ios::in | std::ios::binary );
+}
+
+static std::string read_file_chars( std::ifstream & f, std::uint32_t numb)
 {
-    // Open the stream to 'lock' the file.
-    std::ifstream f(path, std::ios::in | std::ios::binary);
-
     // Create a buffer.
     std::vector<char> result(numb);
     // Read the whole file into the buffer.
--- tests/fileReadTest.cpp
+++ tests/fileReadTest.cpp
@@ -18,17 +18,37 @@ I've had my share of sand kicked in my face
 But I've come through.
 )"
         };
-        auto str = read_file_chars("data/first.in", 200);
+
+        auto fs = open_file("data/first.in");
+        auto str = read_file_chars(fs, 200);
         EXPECT_EQ(test_case, str);
     }
     {
         std::string test_case = {
 R"(I've paid my dues
-Time after time.)"
+Time after time.
+)"
         };
-        auto str = read_file_chars("data/first.in", 34);
+        auto fs = open_file("data/first.in");
+        auto str = read_file_chars(fs, 35);
         EXPECT_EQ(test_case, str);
-    }
 
-//    std::cout << str << std::endl;
+        std::string test_case1 = {
+            R"(I've done my sentence
+)"
+        };
+        auto str1 = read_file_chars(fs, 22);
+        EXPECT_EQ(test_case1, str1);
+
+        std::string test_case2 = {
+            R"(But committed no crime.
+And bad mistakes ?
+I've made a few.
+I've had my share of sand kicked in my face
+But I've come through.
+)"
+        };
+        auto str2 = read_file_chars(fs, 1000);
+        EXPECT_EQ(test_case2, str2);
+    }
 }
